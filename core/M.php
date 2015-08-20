c912f813c2a4528c926c655220fbfe31bcd0bcde
beto <rscattini@descuentocity.com>
2015-08-20 12:39:19 -0300

    -le cambie el nombre para facilitar la escritura

---
+++ core/M.php
@@ -0,0 +1,19 @@
+<?php
+
+class M {
+
+  private $success = null;
+  private $data    = null;
+  private $msg     = null;
+
+  public static function create($success, $data, $msg){
+    // Clone the empty message
+    $scm = clone $this;
+
+    $scm->success = $success;
+    $scm->data    = $data;
+    $scm->msg     = $msg;
+
+    return $scm;
+  }
+}
